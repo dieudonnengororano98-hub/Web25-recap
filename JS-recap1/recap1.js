eac3bc7a170944a9b2f0ce4f6d790b3fa7f18919
dieudonneshaka <dieudonnengororano98@gmail.com>
2026-08-21 13:23:48 +0300

    Add greeting element and cabin class switch statement

--- JS-recap1/recap1.js
+++ JS-recap1/recap1.js
@@ -1,1 +1,25 @@
-alert(1 + 2 +5);
+const cabinClass = prompt("Enter the cabin class (A/B/C).");
+switch (cabinClass) {
+    case 'A':
+        console.log('Top deck cabin with window.');
+        break;
+    case 'B':
+        console.log('Top deck cabin without window.');
+        break;
+    case 'C':
+        console.log('Windowless cabin under the car deck.');
+        break;
+    default:
+        console.log("Invalid cabin class.");
+}
+
+
+
+
+
+
+
+
+
+
+
