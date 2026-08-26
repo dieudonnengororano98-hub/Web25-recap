ba3f1a77a806c472a859a16595f0133d81489792
dieudonneshaka <dieudonnengororano98@gmail.com>
2026-08-26 10:07:28 +0300

    render calculated distance and point coordinates in HTML

--- JS-recap1/recap1.js
+++ JS-recap1/recap1.js
@@ -3,14 +3,23 @@ let celsius = prompt("Enter temperature in Celsius:");
 let fahrenheit = (celsius * 9 / 5) + 32;
 let kelvin = Number(celsius) + 273.15;
 
-    document.getElementById("demo").innerHTML = `
+  document.getElementById("demo").innerHTML = `
   <p>Celsius: ${celsius} °C</p>
   <p>Fahrenheit: ${fahrenheit} °F</p>
   <p>Kelvin: ${kelvin} K</p>
 `;
 
 
-let X1 = prompt("Enter X1");
-let X2 = prompt("Enter X2")
-let Y1 = prompt("Enter Y1");
-let Y2 = prompt("Enter Y2")
+
+let x1 = prompt("Enter x1:");
+let y1 = prompt("Enter y1:");
+let x2 = prompt("Enter x2:");
+let y2 = prompt("Enter y2:");
+
+let distance = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
+
+document.getElementById("donne").innerHTML = `
+  <p>Point 1: (${x1}, ${y1})</p>
+  <p>Point 2: (${x2}, ${y2})</p>
+  <p>Distance: ${distance.toFixed(2)}</p>
+`;
