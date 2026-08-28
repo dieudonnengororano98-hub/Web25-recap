80b445fdaf5c6aa7d5b019e8b7fb014c25829851
dieudonneshaka <dieudonnengororano98@gmail.com>
2026-08-28 21:09:54 +0300

    add sign-up tab switching with vanilla JavaScript

---
+++ Individual/script.js
@@ -0,0 +1,27 @@
+const loginTab = document.getElementById('login-tab');
+const signupTab = document.getElementById('signup-tab');
+
+const firstName = document.querySelector('.first-name');
+const lastName = document.querySelector('.last-name');
+const loginBtn = document.querySelector('.login-1');
+const signupBtn = document.querySelector('.signup-1');
+
+signupTab.addEventListener('click', function() {
+    signupTab.classList.add('active');
+    loginTab.classList.remove('active');
+
+    firstName.classList.remove('hide');
+    lastName.classList.remove('hide');
+    signupBtn.classList.remove('hide');
+    loginBtn.classList.add('hide');
+});
+
+loginTab.addEventListener('click', function() {
+    loginTab.classList.add('active');
+    signupTab.classList.remove('active');
+
+    firstName.classList.add('hide');
+    lastName.classList.add('hide');
+    signupBtn.classList.add('hide');
+    loginBtn.classList.remove('hide');
+});
